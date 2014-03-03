60d766dbf51a93e8c441ae4c2bc9a68e908ed4e5
n2o <cmeter@googlemail.com>
2014-03-03 17:13:54 +0100

    Added a function to get a title of a track from soundcloud

--- src/trickle/core.clj
+++ src/trickle/core.clj
@@ -5,13 +5,22 @@
 
 ;;;; Example to download an image
 ;; (download-file "http://timenewsfeed.files.wordpress.com/2013/12/doge.jpg")
-(defn download-file [url]
-  (let [conn-image (client/get url {:as :byte-array})
-        filename   (last (clojure.string/split url #"/"))]
+(defn download-file [url filename]
+  (let [conn-image (client/get url {:as :byte-array})]
     (with-open [w (clojure.java.io/output-stream filename)]
       (.write w (:body conn-image)))))
+
+(defn get-track-name
+  "Takes a body of a given url and extracts the track name.
+   Returns the name of the track as a string."
+  [body]
+  (clojure.string/replace
+   (clojure.string/replace
+    (first (re-seq #"<title>.+by" body)) #"<title>" "") #" by" ""))
 
 
 ;; Construct download link:
 ;; https://api.soundcloud.com/tracks/DATA-SC-TRACK/download?client_id=32HASH
 ;; https://api.soundcloud.com/tracks/69992039/download?client_id=b45b1aa10f1ac2941910a7f0d10f8e28
+
+
