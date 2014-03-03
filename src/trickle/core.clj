146ba8cee1fdd134629c696597ea3ddfb3b8abef
Alexander Schneider <alex@just-wegi.de>
2014-03-03 15:23:19 +0100

    Add prettyfied example Soundcloud .html file

--- src/trickle/core.clj
+++ src/trickle/core.clj
@@ -1,7 +1,12 @@
 (ns trickle.core
+  (:require [clj-http.client :as client])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn print-track-name
+  [url]
+  (client/get url))
