7a181745836995ef4c2e0cef51345367fb10ee62
Alexander Schneider <alex@just-wegi.de>
2014-03-03 17:23:43 +0100

    Add rudimentary free track download from soundcloud

--- src/trickle/core.clj
+++ src/trickle/core.clj
@@ -11,3 +11,16 @@
     (with-open [w (clojure.java.io/output-stream filename)]
       (.write w (:body conn-image)))))
 
+(defn download-free-track
+  "Download a free track from soundcloud. Still in testing due to unknown client_id"
+  [url target-uri]
+  (let [html ((client/get url) :body)
+        matches (re-seq #"data-sc-track=\"(\d+)\"" html)
+        track-id (second (first matches))]
+    (spit target-uri (client/get
+                      (str "https://api.soundcloud.com/tracks/"
+                           track-id
+                           "/download?client_id=b45b1aa10f1ac2941910a7f0d10f8e28")))))
+
+
+(download-free-track "https://soundcloud.com/theclerkscologne/play-skatalites-garden-of-love" "clerks.wav")
