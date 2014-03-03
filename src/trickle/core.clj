ff9939c98a75b05244f1723b964a0c3770b882c8
n2o <cmeter@googlemail.com>
2014-03-03 15:23:15 +0100

    Added example function to download an image at a given url.

--- src/trickle/core.clj
+++ src/trickle/core.clj
@@ -1,7 +1,12 @@
 (ns trickle.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client])
+  (:require [clojure.string]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;;; Example to download an image 
+(defn download-image [url]
+  (let [conn-image (client/get url {:as :byte-array})
+        filename   (last (clojure.string/split url #"/"))]
+    (with-open [w (clojure.java.io/output-stream filename)]
+      (.write w (:body conn-image)))))
+(download-image "http://timenewsfeed.files.wordpress.com/2013/12/doge.jpg")
